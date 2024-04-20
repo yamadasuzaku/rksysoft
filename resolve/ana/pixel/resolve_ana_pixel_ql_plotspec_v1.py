3d7f5310b8e2c928c5637e2d0cfe5f90a4f525b0
Shinya Yamada <71322538+yamadasuzaku@users.noreply.github.com>
2024-04-21 00:09:32 +0900

    plot pi

---
+++ resolve/ana/pixel/resolve_ana_pixel_ql_plotspec_v1.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from astropy.time import Time
+import datetime
+import argparse
+import sys
+from matplotlib.colors import LogNorm, Normalize
+import matplotlib.cm as cm
+
+# Constants
+MJD_REFERENCE_DAY = 58484
+REFERENCE_TIME = Time(MJD_REFERENCE_DAY, format='mjd')
+TIME_50MK = 150526800.0
+
+# Plotting Configuration
+plt.rcParams['font.family'] = 'serif'
+usercmap = plt.get_cmap('jet')
+cNorm = Normalize(vmin=0, vmax=35)
+scalarMap = cm.ScalarMappable(norm=cNorm, cmap=usercmap)
+
+# Type Information
+itypename = [0, 1, 2, 3, 4]
+typename = ["Hp", "Mp", "Ms", "Lp", "Ls"]
+icol = ["r", "b", "c", "m", "y"]
+ishape = [".", "s", "D", "*", "x"]
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Plot spectra from a FITS file.')
+    parser.add_argument('filename', help='The name of the FITS file to process.')
+    return parser.parse_args()
+
+def open_fits_data(fname):
+    try:
+        return fits.open(fname)[1].data
+    except FileNotFoundError:
+        print("ERROR: File not found", fname)
+        sys.exit()
+
+def process_data(data, TRIGTIME_FLAG=False, AC_FLAG=False):
+    time = data["TRIGTIME"] if TRIGTIME_FLAG else data["TIME"]
+    itype = data["AC_ITYPE"] if AC_FLAG else data["ITYPE"]
+    if len(time) == 0:
+        print("ERROR: data is empty", time)
+        sys.exit()
+    additional_columns = [data[col] for col in ["PI", "RISE_TIME", "DERIV_MAX", "PIXEL"]]
+    sortid = np.argsort(time)
+    sorted_columns = [col[sortid] for col in [time, itype] + additional_columns]
+    dtime = np.array([REFERENCE_TIME.datetime + datetime.timedelta(seconds=float(t)) for t in sorted_columns[0]])
+    print(f"data from {dtime[0]} --> {dtime[-1]}")
+    dt = np.diff(sorted_columns[0])
+    return [column[:-1] for column in sorted_columns], dt
+
+def plot_pi(pi, itype, pixel, outfname="mkpi.png", title="test"):
+    binnum = 1000
+    xmin = 0
+    xmax = 20000
+    
+    for itype_ in itypename:
+        plt.figure(figsize=(11, 7))
+        plt.subplots_adjust(right=0.8) # make the right space bigger
+        plt.xscale("linear")
+        plt.yscale("log")
+        plt.ylabel("Counts/bin")
+        plt.xlabel("PI (eV)")
+        plt.grid(alpha=0.8)
+        plt.title(title + " TYPE = " + typename[itype_])
+
+        # Filter data by itype
+        typecut = (itype == itype_)
+        pi_filtered = pi[typecut]
+
+        # Compute histogram for all pixels of current itype
+        hist, binedges = np.histogram(pi_filtered, bins=binnum, range=(xmin, xmax))
+        bincenters = 0.5 * (binedges[1:] + binedges[0:-1])
+        ene = bincenters * 0.5 + 0.5
+        event_number = len(pi_filtered)
+        plt.errorbar(ene, hist, yerr=np.sqrt(hist), color="k", fmt='-', label="all" + "("+str(event_number)+ "c)", alpha=0.5)
+
+        # Plot histograms for each pixel
+        for pixel_ in np.arange(36):
+            pixelcut = (pixel == pixel_) & typecut
+            pi_pixel_filtered = pi[pixelcut]
+            
+            if len(pi_pixel_filtered) == 0:
+                print("warning: data is empty for pixel =", pixel_)
+                continue
+
+            hist, binedges = np.histogram(pi_pixel_filtered, bins=binnum, range=(xmin, xmax))
+            ene = 0.5 * (binedges[1:] + binedges[:-1]) * 0.5 + 0.5
+            color = scalarMap.to_rgba(pixel_)
+            event_number = len(pi_pixel_filtered)
+            plt.errorbar(ene, hist, yerr=np.sqrt(hist), color=color, fmt='-', label=f"P{pixel_}" + "("+str(event_number)+ "c)")
+
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0., fontsize=6)
+        plt.xlim(0, 10000)
+        ofname = f"fig_{typename[itype_]}_{outfname}"
+        plt.savefig(ofname)
+        plt.show()
+        print(f"..... {ofname} is created.")
+
+def main():
+    args = parse_arguments()
+    data = open_fits_data(args.filename)
+    processed_data, dt = process_data(data)
+    time, itype, pi, rise_time, deriv_max, pixel = processed_data  # data unpack
+    plot_pi(pi, itype, pixel, outfname=f"ql_plotspec_{args.filename.replace('.evt', '').replace('.gz', '')}.png", title=f"Spectra of {args.filename}")
+
+if __name__ == "__main__":
+    main()
