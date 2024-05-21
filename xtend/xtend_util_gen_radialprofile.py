dd1845ba1a8cc51cf92354cb30701c978f1efd50
Shinya Yamada <71322538+yamadasuzaku@users.noreply.github.com>
2024-05-21 18:55:35 +0900

    sakai-kun

--- xtend/xtend_util_gen_radialprofile.py
+++ xtend/xtend_util_gen_radialprofile.py
@@ -15,10 +15,34 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as cr
 from astropy.io import fits
 from astropy.wcs import WCS
+from scipy.signal import convolve
+
 
 params = {'xtick.labelsize': 10, 'ytick.labelsize': 10, 'legend.fontsize': 8}
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams.update(params)
+
+def calc_pileupfraction(r):
+    r = np.asarray(r)  # rをnumpy配列に変換（既に配列の場合はそのまま）
+    result = np.where(r < 0, 0.0, (1. - (1. + r) * np.exp(-r)) / (1. - np.exp(-r)))
+    return result
+
+def interpolate_centers(radial_centers, radial_pileupfraction, target_values = [0.03, 0.01]):
+
+    result = []
+    
+    for target in target_values:
+        interp_center = -1
+        for i in range(1, len(radial_pileupfraction)):
+            if (radial_pileupfraction[i-1] >= target >= radial_pileupfraction[i] or
+                radial_pileupfraction[i-1] <= target <= radial_pileupfraction[i]):
+                interp_center = np.interp(target, 
+                                          [radial_pileupfraction[i-1], radial_pileupfraction[i]], 
+                                          [radial_centers[i-1], radial_centers[i]])
+                break
+        result.append(interp_center)
+    
+    return result    
 
 class Fits:
     """Class for handling FITS files and plotting radial profiles."""
@@ -76,21 +100,22 @@ class Fits:
     def plotwcs(self, x_center, y_center, vmin=1e-1, vmax=20, manual=False):
         """Plot the entire image with WCS."""
         print("\n[def plotwcs]")
-
         if not manual:
             vmin = np.amin(self.hdu.data[self.hdu.data > 0]) + 0.1  # Avoid vmin being too close to 0 for LogNorm
             vmax = np.amax(self.hdu.data)
         print("     vmin = ", vmin, "vmax = ", vmax)
-    
+
+        print("vmin",vmin, "vmax",vmax)
         plt.figure(figsize=(12, 8))
         plt.figtext(0.1, 0.95, f"OBSID={self.obsid} {self.object} mode={self.datamode} exp={self.exposure} unit={self.p2arcsec}[arcsec/pixel] ")
-        plt.figtext(0.1, 0.91, f"Input center [deg] (x, y) = ({x_center:.4f}, {y_center:.4f})")
+        plt.figtext(0.05, 0.025, f"fname={self.filename}")
 
-        img = plt.imshow(self.data, origin='lower', cmap=plt.cm.jet, norm=cr.LogNorm(vmin=vmin, vmax=vmax))
+        img = plt.imshow(self.data, origin='lower', cmap=plt.cm.jet, norm=cr.LogNorm(vmin=vmin, vmax=vmax))        
+        plt.scatter(x_center, y_center, c="k", s=300, marker="x", alpha=0.3)
+
         cbar = plt.colorbar(img)
         cbar.set_label('Intensity (c/bin)', rotation=270, labelpad=15)
 
-        plt.scatter(x_center, y_center, c="k", s=300, marker="x", alpha=0.3)
         plt.xlabel('RA')
         plt.ylabel('Dec')
 
@@ -118,7 +143,8 @@ class Fits:
 
         plt.figure(figsize=(12, 8))
         plt.figtext(0.1, 0.95, f"OBSID={self.obsid} {self.object} mode={self.datamode} exp={self.exposure} unit={self.p2arcsec}[arcsec/pixel] ")
-        plt.figtext(0.1, 0.91, f"Input center [deg] (x, y, ra, dec) = ({x_center:.4f}, {y_center:.4f}, {ra:.4f}, {dec:.4f})")
+        plt.figtext(0.05, 0.025, f"fname={self.filename}")
+
         ax = plt.subplot(111, projection=self.wcs)
         img = plt.imshow(self.data, origin='lower', cmap=plt.cm.jet, norm=cr.LogNorm(vmin=vmin, vmax=vmax))
         ax.scatter(x_center, y_center, c="k", s=300, marker="x", alpha=0.3)
@@ -136,7 +162,7 @@ class Fits:
         plt.savefig(outputfigname)
         print(f"..... {outputfigname} is created.")
 
-    def mk_radialprofile(self, x_center, y_center, search_radius=40, ndiv=20, vmin=1e-1, vmax=20, manual=False):
+    def mk_radialprofile(self, x_center, y_center, search_radius=40, ndiv=20, vmin=1e-1, vmax=20, manual=False, cellsize=3*3):
         """Create the radial profiles."""
         print("\n[def mk_radialprofile]")
         if not manual:
@@ -153,43 +179,65 @@ class Fits:
         print(f"x_center, y_center = {x_center}, {y_center}")
         print(f"ra, dec    = {ra}, {dec}")
 
-        rc, rp = calc_radial_profile(self.data, x_center, y_center, search_radius=search_radius, ndiv=ndiv)
-
+        rc, rp = calc_radial_profile(self.data, x_center, y_center, search_radius=search_radius, ndiv=ndiv, exposure=self.exposure)
+        radial_centers, radial_pileupfraction = calc_radial_pileupfraction(self.data, x_center, y_center, exposure=self.exposure, search_radius=search_radius, ndiv=ndiv, cellsize=cellsize)
+        pileupfraction_img = calc_2d_pileupfraction(self.data, exposure=self.exposure, cellsize=cellsize)
+        
+        target_values = [0.03, 0.01] # pileup fraction 3%, 1%
+        pileup_result = interpolate_centers(radial_centers, radial_pileupfraction, target_values = target_values) 
         # Plot images and radial profiles
-        fig = plt.figure(figsize=(12, 8))
-        fig.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.87, wspace=0.3, hspace=0.3)
+        fig = plt.figure(figsize=(16, 8))
+        fig.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.87, wspace=0.5, hspace=0.4)
         plt.figtext(0.1, 0.95, f"OBSID={self.obsid} {self.object} mode={self.datamode} exp={self.exposure} unit={self.p2arcsec}[arcsec/pixel] ")
-        plt.figtext(0.1, 0.93, f"Input center [deg] (x, y, ra, dec) = ({x_center:.4f}, {y_center:.4f}, {ra:.4f}, {dec:.4f})")
+        plt.figtext(0.1, 0.93, f"Input center [deg] (x, y, ra, dec) = ({x_center:.4f}, {y_center:.4f}, {ra:.4f}, {dec:.4f}) cellsize for pileup = {cellsize}")
+        plt.figtext(0.05, 0.025, f"fname={self.filename}")
 
-        ax1 = fig.add_subplot(2, 3, 1)
+        ax1 = fig.add_subplot(2, 4, 1)
         self._plot_image(ax1, self.data, "(1) SKY image", x_center, y_center, vmin, vmax)
 
-        ax2 = fig.add_subplot(2, 3, 4, projection=self.wcs)
+        ax2 = fig.add_subplot(2, 4, 5, projection=self.wcs)
         self._plot_image(ax2, self.data, "(2) Ra Dec image (FK5)", x_center, y_center, vmin, vmax, wcs=self.wcs)
 
-        ax3 = fig.add_subplot(2, 3, 2)
+        ax3 = fig.add_subplot(2, 4, 2)
         self._plot_image(ax3, self.data, "(3) SKY image", x_center, y_center, vmin, vmax, search_radius)
 
-        ax4 = fig.add_subplot(2, 3, 5, projection=self.wcs)
+        ax4 = fig.add_subplot(2, 4, 6, projection=self.wcs)
         self._plot_image(ax4, self.data, "(4) Ra Dec image (FK5)", x_center, y_center, vmin, vmax, search_radius, wcs=self.wcs)
 
-        ax5 = fig.add_subplot(2, 3, 3)
+        ax5 = fig.add_subplot(2, 4, 3)
         self._plot_radial_profile(ax5, rc, rp, "(5) Radial profile (pix)", 'radial distance (pixel)', 'c s$^{-1}$ pixel$^{-2}$')
 
-        ax6 = fig.add_subplot(2, 3, 6)
-        self._plot_radial_profile(ax6, rc * self.p2arcsec, rp, "(6) Radial profile (arcsec)", 'radial distance (arcsec)', 'c s$^{-1}$ pixel$^{-2}$')
+        ax6 = fig.add_subplot(2, 4, 7)
+        self._plot_radial_profile(ax6, radial_centers, radial_pileupfraction, "(6) Pileup Fraction", 'radial distance (pixel)', 'fraction')
+        for _target, _r in zip(target_values,pileup_result):
+            if _r > 0:
+                print(f"***** pileup fraction ={_target*100:.2f} % at {_r:.2f} pixel *****")
+                ax6.axhline(y=_target, color='r', ls="--", alpha=0.3, label=f"plfrac={_target*100:.2f} % at {_r:.2f} pixel")
+                plt.legend(numpoints=1, frameon=True)
+#        ax6 = fig.add_subplot(3, 2, 6)
+#        self._plot_radial_profile(ax6, rc * self.p2arcsec, rp, "(6) Radial profile (arcsec)", 'radial distance (arcsec)', 'c s$^{-1}$ deg$^{-2}$')
+
+        ax7 = fig.add_subplot(2, 4, 4)
+        self._plot_image(ax7, pileupfraction_img, "(7) Pileup Fraction image", x_center, y_center, 1e-4, np.nanmax(pileupfraction_img), search_radius, label='fraction')
+
+        ax8 = fig.add_subplot(2, 4, 8)
+        self._plot_image(ax8, pileupfraction_img, "(8) Pileup Fraction image (zoom in)", x_center, y_center, 1e-2, np.nanmax(pileupfraction_img), int(search_radius/6), label='fraction')
+
 
         outputfigname = f"{self.dirname}_radialprofile_xcenter{x_center}_ycenter{y_center}.png"
         plt.savefig(outputfigname)
         print(f"..... {outputfigname} is created.")
 
         self._save_radial_profile(rc, rp, f"{self.dirname}_radialprofile_xcenter{x_center}_ycenter{y_center}.txt")
-
-    def _plot_image(self, ax, data, title, x_center, y_center, vmin, vmax, search_radius=None, wcs=None):
-        ax.set_title(title)        
+        self._save_radial_profile(radial_centers, radial_pileupfraction, f"{self.dirname}_pileupfraction_xcenter{x_center}_ycenter{y_center}.txt")
+        self._save_pileup_oneline(pileup_result, f"{self.dirname}_pileupfraction_xcenter{x_center}_ycenter{y_center}.csv")
+        
+    def _plot_image(self, ax, data, title, x_center, y_center, vmin, vmax, search_radius=None, wcs=None, label=None):
+        ax.set_title(title)
         im = ax.imshow(data, origin='lower', cmap=plt.cm.jet, norm=cr.LogNorm(vmin=vmin, vmax=vmax), aspect='auto', extent=[0, data.shape[1], 0, data.shape[0]])
-        plt.colorbar(im, ax=ax)
-        ax.scatter(x_center, y_center, c="k", s=300, marker="x")
+        cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+        cbar.set_label(label, rotation=270, labelpad=15)
+        ax.scatter(x_center, y_center, c="k", s=300, marker="x",alpha=0.3)
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         if search_radius is not None:
@@ -205,15 +253,87 @@ class Fits:
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
         ax.grid(True)
-        ax.legend(numpoints=1, frameon=False)
+        ax.legend(numpoints=1, frameon=True)
 
     def _save_radial_profile(self, rc, rp, outputcsv):
         with open(outputcsv, "w") as fout:
-            for onex, oney in zip(rc * self.p2arcsec, rp):
-                fout.write(f"{onex},{oney}\n")
+            for _rc, _rcsec, _rp in zip(rc, rc * self.p2arcsec, rp):
+                fout.write(f"{_rc},{_rcsec},{_rp}\n")
         print(f"..... {outputcsv} is created.")
 
-def calc_radial_profile(data, x_center, y_center, search_radius=10, ndiv=10, exposure=1e4, debug=False):
+    def _save_pileup_oneline(self, pileup_result, outputcsv):
+        rough_rate = np.sum(self.data)/self.exposure
+        with open(outputcsv, "w") as fout:
+            fout.write(f"{self.obsid},{pileup_result[0]},{pileup_result[1]},{self.object},{self.datamode},{self.exposure},{rough_rate}\n")
+        print(f"..... {outputcsv} is created.")
+
+def calc_radial_pileupfraction(data, x_center, y_center, search_radius=10, ndiv=10, exposure = 1e4, cellsize=3*3, debug=False):
+    """
+    Calculate the radial pileup fraction around a given center.
+
+    Parameters:
+    data (2D array): The input data array.
+    x_center (int): The x-coordinate of the center.
+    y_center (int): The y-coordinate of the center.
+    search_radius (int): The radius around the center to search for the profile. Default is 10.
+    ndiv (int): The number of divisions in the radial profile. Default is 10.
+    exposure (float): The exposure time to calculate pileup fraction. Default is 1e4.
+    cellsize (float): The cell size for single phtoon. Default is 3x3, though need to check. 
+    debug (bool): If True, print debug information. Default is False.
+
+    Returns:
+    tuple: The radial coordinates and the normalized radial profile.
+    """
+    transposed_data = data.T
+    radial_bins = np.linspace(0, search_radius, ndiv)
+    radial_centers = 0.5 * (radial_bins[:-1] + radial_bins[1:])
+    radial_profile = np.zeros(len(radial_bins) - 1)
+
+    x_indices, y_indices = np.meshgrid(np.arange(2 * search_radius), np.arange(2 * search_radius), indexing='ij')
+    x_indices = x_center - search_radius + x_indices
+    y_indices = y_center - search_radius + y_indices
+
+    distances = dist2d(x_indices, y_indices, x_center, y_center)
+    values = transposed_data[x_indices, y_indices]
+
+    for k in range(len(radial_bins) - 1):
+        mask = (distances >= radial_bins[k]) & (distances < radial_bins[k + 1])
+        radial_profile[k] = np.sum(values[mask])
+
+    # Normalize the radial profile and calculate pileup fraction 
+    areas = np.pi * (radial_bins[1:]**2 - radial_bins[:-1]**2)
+    radial_profile_per_areas_exposure = radial_profile * cellsize / (areas * exposure)
+    radial_pileupfraction = calc_pileupfraction(radial_profile_per_areas_exposure)
+
+    if debug:
+        for m in range(len(radial_bins) - 1):
+            print(m, radial_profile[m], radial_pileupfraction[m], radial_bins[m], radial_bins[m+1], areas[m])
+
+    return radial_centers, radial_pileupfraction
+
+def calc_2d_pileupfraction(data, exposure=1e4, cellsize=3*3, debug=False):
+    """
+    Calculate the pileup from image information.
+
+    Parameters:
+    data (2D array): The input data array.
+    exposure (float): The exposure time to calculate pileup fraction. Default is 1e4.
+    cellsize (float): The cell size for single phtoon. Default is 3x3, though need to check. 
+    debug (bool): If True, print debug information. Default is False.
+
+    Returns:
+    2D array: The pileup 2d image.
+    """
+    cellsize_kernel = np.ones((3,3), dtype=data.dtype)
+    cellsize_per_exposure = convolve(data, cellsize_kernel, mode='same') / exposure
+    pileupfraction_img = calc_pileupfraction(cellsize_per_exposure)
+
+    if debug:
+        print("vmin = ", np.max(pileupfraction_img), "vmax = ", np.max(pileupfraction_img), "mean = ", np.mean(pileupfraction_img))
+
+    return pileupfraction_img
+
+def calc_radial_profile(data, x_center, y_center, search_radius=10, ndiv=10, exposure = 1e4, debug=False):
     """
     Calculate the radial profile of the data around a given center.
 
@@ -223,7 +343,6 @@ def calc_radial_profile(data, x_center, y_center, search_radius=10, ndiv=10, exp
     y_center (int): The y-coordinate of the center.
     search_radius (int): The radius around the center to search for the profile. Default is 10.
     ndiv (int): The number of divisions in the radial profile. Default is 10.
-    exposure (float): The exposure time to calculate pileup fraction. Default is 1e4.    
     debug (bool): If True, print debug information. Default is False.
 
     Returns:
@@ -247,7 +366,7 @@ def calc_radial_profile(data, x_center, y_center, search_radius=10, ndiv=10, exp
 
     # Normalize the radial profile
     areas = np.pi * (radial_bins[1:]**2 - radial_bins[:-1]**2)
-    normalized_radial_profile = radial_profile / (areas*exposure)
+    normalized_radial_profile = radial_profile / (areas * exposure) 
 
     if debug:
         for m in range(len(radial_bins) - 1):
@@ -313,8 +432,9 @@ def main():
     parser.add_argument('-m', '--manual', action='store_true', help='Flag to use vmax, vmin', default=False)
     parser.add_argument('-x', '--x_center', type=int, help='x coordinate of center', default=1215)
     parser.add_argument('-y', '--y_center', type=int, help='y coordinate of center', default=1215)
-    parser.add_argument('-a', '--vmax', type=float, help='VMAX', default=400)
-    parser.add_argument('-i', '--vmin', type=float, help='VMIN', default=0.9)
+    parser.add_argument('-c', '--cellsize', type=int, help='cell size for a single event', default=3*3)
+    parser.add_argument('-a', '--vmax', type=float, help='VMAX', default=3e3)
+    parser.add_argument('-i', '--vmin', type=float, help='VMIN', default=1)
     parser.add_argument('-s', '--search_radius', type=int, help='Search radius for data extraction', default=80)
     parser.add_argument('-n', '--numberOfDivision', type=int, help='Number of divisions for annulus', default=20)
     parser.add_argument('filename', help='Input FITS file')
@@ -324,7 +444,7 @@ def main():
     print(f"---------------------------------------------\n| START  :  {__file__}")
     print(f"..... auto_updated_center {options.auto_updated_center}")
     print(f"..... x_center {options.x_center} y_center {options.y_center}")
-    print(f"..... search_radius {options.search_radius} (bin of image) ndiv {options.numberOfDivision}")
+    print(f"..... search_radius {options.search_radius} (bin of image) ndiv {options.numberOfDivision} cellsize {options.cellsize}")
     print(f"..... debug {options.debug} manual {options.manual}")
     print(f"..... vmax {options.vmax} vmin {options.vmin}")
 
@@ -342,7 +462,9 @@ def main():
 
     eve.plotwcs(x_center, y_center, vmax=options.vmax, vmin=options.vmin, manual=options.manual)
     eve.plotwcs_ps(x_center, y_center, search_radius=options.search_radius, vmax=options.vmax, vmin=options.vmin, manual=options.manual)
-    eve.mk_radialprofile(x_center, y_center, search_radius=options.search_radius, ndiv=options.numberOfDivision, vmax=options.vmax, vmin=options.vmin, manual=options.manual)
+    eve.mk_radialprofile(x_center, y_center, search_radius=options.search_radius, \
+                           ndiv=options.numberOfDivision, vmax=options.vmax, vmin=options.vmin, \
+                           manual=options.manual, cellsize=options.cellsize)
 
     print("| finish \n")
 
