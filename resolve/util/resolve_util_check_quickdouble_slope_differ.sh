27b0319b0a151c46d872e1e16ec745a6f0971103
Shinya Yamada <71322538+yamadasuzaku@users.noreply.github.com>
2024-06-04 11:44:20 +0900

    modheader

--- resolve/util/resolve_util_check_quickdouble_slope_differ.sh
+++ resolve/util/resolve_util_check_quickdouble_slope_differ.sh
@@ -9,6 +9,6 @@ fi
 obs=$1
 ftlist $obs+1 K include=OBJECT
 ftlist $obs+1 K include=DATE-OBS,DATE-END
-ftlist $obs+1 K include=PROCVE,TLM2FITS,SOFTVER,CALDBVER
+ftlist $obs+1 K include=PROCVER,DATE,TLM2FITS,SOFTVER,CALDBVER
 echo COUNTS QUICK_DOUBLE SLOPE_DIFFER;
 ftlist $obs T column=QUICK_DOUBLE,SLOPE_DIFFER | awk 'NR>3 {print $2,$3}' | sort | uniq -c
