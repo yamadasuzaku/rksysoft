5783be74b60fbe2e4178cf9112ba203848561024
Shinya Yamada <71322538+yamadasuzaku@users.noreply.github.com>
2024-06-10 02:30:50 +0900

    hk

--- resolve/ana/pixel/resolve_run_ana_pixel_Ls_mksubgroup.sh
+++ resolve/ana/pixel/resolve_run_ana_pixel_Ls_mksubgroup.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+# スクリプトに渡された引数を取得
+input_file="$1"
+
+# 引数が渡されているか確認
+if [ -z "$input_file" ]; then
+  echo "Usage: $0 <input_file>"
+  exit 1
+fi
+
 for i in `seq 0 35`
 do
 echo ".............. start"  
 echo PIXEL = $i 
-resolve_ana_pixel_Ls_mksubgroup.py xa000114000rsl_p0px1000_uf_prevnext_cutclgti.fits TIME PREV_INTERVAL -f "PIXEL==${i}"
+resolve_ana_pixel_Ls_mksubgroup.py $input_file TIME PREV_INTERVAL -f "PIXEL==${i}"
 echo ".............. finish"  
 done
