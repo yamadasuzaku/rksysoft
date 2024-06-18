dc3c83b6103c4aa9f09af3653dd19ed6601199c4
Shinya Yamada <71322538+yamadasuzaku@users.noreply.github.com>
2024-06-19 01:58:36 +0900

    gti

--- resolve/util/resolve_util_gtiplot.py
+++ resolve/util/resolve_util_gtiplot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+#!/usr/bin/env python
+
 import argparse
 import matplotlib.pyplot as plt
 import sys
@@ -19,28 +21,101 @@ from matplotlib.lines import Line2D
 MJD_REFERENCE_DAY = 58484
 REFERENCE_TIME = Time(MJD_REFERENCE_DAY, format='mjd')  # Reference time in Modified Julian Day
 
-def plot_fits_data(file_names, title, outfname, \
-                      plotflag = False, markers = "o", debug=True, markersize = 1):
+def get_file_list(input_arg):
+    # Check if input_arg is a file list indicator
+    print(input_arg, input_arg)
+    if input_arg.startswith('@'):
+        file_list_path = input_arg[1:]
+        with open(file_list_path, 'r') as file:
+            file_list = [line.strip() for line in file.readlines()]
+    else:
+        # Assume it's a comma-separated list of file names or a single file name
+        file_list = input_arg.split(',')
+    
+    return file_list
+
+# Define a function to compute a fast light curve
+def fast_lc(tstart, tstop, binsize, x):
+    """Compute a fast light curve using a given time series."""
+    times = np.arange(tstart, tstop, binsize)[:-1]
+    n_bins = len(times)
+
+    # Initialize arrays for the light curve data
+    x_lc, y_lc = np.zeros(n_bins), np.zeros(n_bins)
+    x_err = np.full(n_bins, 0.5 * binsize)
+    y_err = np.zeros(n_bins)
+
+    # Ensure the time array is sorted
+    x = np.sort(x)
+
+    # Compute counts for each bin
+    for i, t in enumerate(times):
+        start, end = np.searchsorted(x, [t, t + binsize])
+        count = end - start
+
+        x_lc[i] = t + 0.5 * binsize
+        y_lc[i] = count / binsize
+        y_err[i] = np.sqrt(count) / binsize
+
+    return x_lc, x_err, y_lc, y_err
+
+
+def plot_fits_data(gtifiles, evtfiles, title, outfname, \
+                      plotflag = False, markers = "o", debug=True, markersize = 1, timebinsize = 100.):
 
     # GTI データを格納する辞書
     gtidic = {}
     index = 0
+    objlist = []
 
-    for file_name in file_names:
-        with fits.open(file_name) as hdulist:
+    for gtifile in gtifiles:
+        with fits.open(gtifile) as hdulist:
             for hdu in hdulist:
                 if 'GTI' in hdu.name:
                     print(f"..... {hdu.name} is opened.")
+                    obj = hdu.header["OBJECT"]
+                    objlist.append(obj)
                     start = hdu.data["START"]
                     stop = hdu.data["STOP"]
                     # 辞書にデータを格納
                     gtidic[index] = {
-                        'file_name': file_name,
+                        'file_name': gtifile,
                         'hdu_name': hdu.name,
                         'start': start,
                         'stop': stop
                     }
                     index += 1
+
+    # make lightcurves 
+    lcdic = {}
+    lcindex = 0
+    if evtfiles == None:
+        pass
+    else:
+        for evtfile in evtfiles:
+            with fits.open(evtfile) as hdulist:
+                for hdu in hdulist:
+                    if 'EVENTS' in hdu.name:	
+                        time = hdu.data["TIME"]        
+                        itype = hdu.data["ITYPE"]        
+                        cutid = np.where(itype<5)[0]
+                        time = time[cutid]
+                        itype = itype[cutid]
+                        x_lc, x_err, y_lc, y_err = fast_lc(time[0], time[-1], timebinsize, time)
+                        zcutid = np.where(y_lc > 0)[0]
+                        x_lc  = x_lc[zcutid]
+                        x_err = x_err[zcutid]
+                        y_lc  = y_lc[zcutid]
+                        y_err = y_err[zcutid]           
+                        dtime_lc = [REFERENCE_TIME.datetime + datetime.timedelta(seconds=float(date_sec)) for date_sec in x_lc]
+                        lcdic[lcindex] = {
+                        'file_name': evtfile,
+                        'datetime': dtime_lc,
+                        'y_lc': y_lc,
+                        'y_err': y_err
+                        }
+                        lcindex += 1
+
 
     fig, axs = plt.subplots(1, 1, figsize=(12, 7))
     plt.subplots_adjust(right=0.75)  # make the right space bigger
@@ -56,108 +131,32 @@ def plot_fits_data(file_names, title, outfname, \
         sdate = np.array([REFERENCE_TIME.datetime + datetime.timedelta(seconds=float(t)) for t in data['start']])
         edate = np.array([REFERENCE_TIME.datetime + datetime.timedelta(seconds=float(t)) for t in data['stop']])
         for s,e in zip(sdate, edate):
-            plt.plot([s, e], [yval,yval], marker='o', ms=2, color=colors[idx])
+            axs.plot([s, e], [yval,yval], marker='o', ms=2, color=colors[idx],alpha=0.8)
         shortfname = os.path.basename(data['file_name'])
-#        plt.plot([], [], ' ', label=shortfname+":"+data['hdu_name'])
-#        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0., fontsize=6)
         # 凡例要素を追加
         legend_elements.append(Line2D([0], [0], color=colors[idx], marker=markers, label=shortfname+":"+data['hdu_name']))
-    # 凡例をプロット
+
     axs.legend(handles=legend_elements, bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0., fontsize=6)
 
-    plt.show()
+    if evtfiles is not None:
+        ax2 = axs.twinx()  # Create a second y-axis
+        for idx, data in lcdic.items():
+            print(f"Index: {idx}, File: {data['file_name']}, datetime: {data['datetime']}, y_lc: {data['y_lc']}, y_err: {data['y_err']}")
+            shortfname = os.path.basename(data['file_name'])
+            ax2.errorbar(data['datetime'], data['y_lc'], yerr=data['y_err'], fmt='.', ms=2, color=colors[idx], label=shortfname)
+        # 凡例をプロット
+        ax2.legend(bbox_to_anchor=(1.05, 0.9), loc='upper left', borderaxespad=0., fontsize=6)
+        ax2.set_ylabel(f"c/s (binsize={timebinsize}s), ITYPE < 5")
 
-
-
-    # for file_name in file_names:
-    #     # Create subplots
-    #     with fits.open(file_name) as hdulist:
-    #         for hdu in hdulist:
-    #             if 'GTI' in hdu.name:
-    #                 print(f"..... {hdu.name} is opened.")
-    #                 start = hdu.data["START"]
-    #                 stop = hdu.data["START"]
-
-    # # GTI データを格納する辞書
-    # gtidic = {}
-    # for file_name in file_names:
-    #     with fits.open(file_name) as hdulist:
-    #         for hdu in hdulist:
-    #             if 'GTI' in hdu.name:
-    #                 print(f"..... {hdu.name} is opened.")
-    #                 start = hdu.data["START"]
-    #                 stop = hdu.data["STOP"]
-    #                 # 辞書にデータを格納
-    #                 if file_name not in gtidic:
-    #                     gtidic[file_name] = []
-    #                 gtidic[file_name].append({
-    #                     'hdu_name': hdu.name,
-    #                     'start': start,
-    #                     'stop': stop
-    #                 })
-
-    # # gtidic の内容を表示
-    # for file_name, data in gtidic.items():
-    #     for entry in data:
-    #         print(f"File: {file_name}, HDU: {entry['hdu_name']}, START: {entry['start']}, STOP: {entry['stop']}")
-
-
-    # for file_name in file_names:
-    #     # Create subplots
-    #     with fits.open(file_name) as hdulist:
-    #         for hdu in hdulist:
-    #             if 'GTI' in hdu.name:
-    #                 print(f"..... {hdu.name} is opened.")
-    #                 start = hdu.data["START"]
-    #                 stop = hdu.data["START"]
-
-    #                 for s,e in zip(start,stop):
-    #                     plt.plot([s, e], [1, 1], marker='o')
-    #                 plt.plot([], [], ' ', label=hdu.name)
-    #                 plt.legend()
-
-    #             else:
-    #                 print(f"..... {hdu.name} is skipped, continue.")
-    #                 continue
-    # plt.show()
-    #         # Apply filters if any
-    #         # Get X data
-    #         x_data = {}
-    #         for ycol, xhdu in zip(y_cols,x_hdus):
-    #             print(f"..... {x_col} is opened from HDU={xhdu}")
-    #             data = hdul[xhdu].data
-    #             if filters:
-    #                 print("..... filters applied")
-    #                 data = apply_filters(data, filters)
-    #             x_data[ycol] = data[x_col]
-
-    #         # Get Y data
-    #         y_data = {}
-    #         for ycol, yhdu in zip( y_cols, y_hdus):
-    #             print(f"..... {ycol} is opened from HDU={yhdu}")
-    #             data = hdul[yhdu].data
-    #             if filters:
-    #                 print("..... filters applied")
-    #                 data = apply_filters(data, filters)
-    #             y_data[ycol] = data[ycol]
-
-    #         if num_plots == 1:
-    #             axs = [axs]  # Ensure axs is always a list for consistency
-
-    #         for ax, y_col, yscale in zip(axs, y_cols, y_scales):
-    #             ax.plot(x_data[y_col], y_data[y_col], markers, label=y_col, markersize = markersize)
-    #             ax.set_ylabel(y_col)
-    #             ax.set_yscale(yscale)
-    #             ax.legend()
-
-    #         axs[-1].set_xlabel(x_col)  # Set x-axis label only on the bottom plot
-    #         plt.suptitle(title)
-
-    # plt.tight_layout()
-    # plt.savefig(outfname)
-    # print(f".....{outfname} is saved.")
-    # if plotflag:
-    #     plt.show()
+    objset = set(objlist)
+    plt.title(",".join(objset))
+    axs.set_ylabel("a.u.")
+    axs.set_xlabel("TIME")
+    plt.tight_layout()
+    plt.savefig(outfname)
+    print(f".....{outfname} is saved.")
+    if plotflag:
+        plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -167,18 +166,24 @@ if __name__ == "__main__":
         resolve_util_gtiplot.py xa300049010rsl_p0px3000_uf_prevnext_cutclgti.fits PHA 1,1 PI,EPI 1,1 --plot --filters itype==0
     ''',
     formatter_class=argparse.RawDescriptionHelpFormatter)    
-    parser.add_argument("file_names", type=str, help="List of Path to the FITS file")
+    parser.add_argument("gtifilelist", type=str, help="Comma-separated GTI file names or a file containing GTI file names.")
     parser.add_argument('--plot', '-p', action='store_true', default=False, help='Flag to display plot')
     parser.add_argument("--markers", '-m', type=str, help="marker type", default="o")
     parser.add_argument("--markersize", '-k', type=float, help="marker size", default=1)
     parser.add_argument("--y_cols_scale", '-s', type=str, help="Comma-separated column names for the y-axis",default=None)
-    parser.add_argument("--gtifiles", type=str, help="Comma-separated column names for gtifiles",default=None)
+    parser.add_argument("--evtfilelist", '-e', type=str, help="Comma-separated evt file names or a file containing evt file names.",default=None)
 
     args = parser.parse_args()
-    file_names = [_ for _ in args.file_names.split(",")]
-    print(f'file_names = {file_names}')
-    title = f"{args.file_names}"
+    gtifiles = get_file_list(args.gtifilelist)
+    print(f'gtifiles = {gtifiles}')        
+    title = ",".join(gtifiles)
 
-    outfname = "gtiplot_" + args.file_names.replace(",","_").replace(".","_p_") + ".png"
+    if args.evtfilelist == None:
+        evtfiles = None
+    else:
+        evtfiles = get_file_list(args.evtfilelist)
+    print(f'evtfiles = {evtfiles}')        
 
-    plot_fits_data(file_names, title, outfname, plotflag = args.plot, markers = args.markers, markersize = args.markersize)
+    outfname = "gtiplot_" + ("_".join(gtifiles)).replace(".","_p_") + ".png"
+
+    plot_fits_data(gtifiles, evtfiles, title, outfname, plotflag = args.plot, markers = args.markers, markersize = args.markersize)
