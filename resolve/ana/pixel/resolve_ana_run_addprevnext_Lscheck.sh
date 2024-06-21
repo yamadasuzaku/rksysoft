dfdb49aab058a190bc10874aeede0478e9f74784
Shinya Yamada <71322538+yamadasuzaku@users.noreply.github.com>
2024-06-21 10:16:23 +0900

    tmp

--- resolve/ana/pixel/resolve_ana_run_addprevnext_Lscheck.sh
+++ resolve/ana/pixel/resolve_ana_run_addprevnext_Lscheck.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# 引数が指定されているか確認
 if [ $# -eq 0 ]; then
     echo "Error: No input file specified."
     echo "Usage: $0 <input_file_uf.evt>"
     exit 1
 fi
 
-# 引数を受け取る
 input_file=$1
 
-# ファイル名の一部を抽出
 base_name=$(basename "$input_file" .evt)
 base_top=$(basename "$input_file" _uf.evt)
 
-# コマンドを順に実行し、進行状況を表示
 echo "Running resolve_tool_addcol_prev_next_interval.py..."
 resolve_tool_addcol_prev_next_interval.py "${base_name}.evt" -o "${base_name}_prevnext.evt"
 echo "Completed: resolve_tool_addcol_prev_next_interval.py"
