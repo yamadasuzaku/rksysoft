4f86e04b60fed9fade38f3c71e75c7b934342749
Shinya Yamada <71322538+yamadasuzaku@users.noreply.github.com>
2024-11-13 12:58:11 +0900

    saa

---
+++ resolve/util/resolve_util_extract_saa_from_ufevt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "[INFO] Starting the script..."
+
+if [ $# -lt 2 ]; then
+    echo "[ERROR] Invalid number of arguments."
+    echo "Usage: $0 <clevt> <ufevt> "
+    echo "Example: $0 xa300041010rsl_p0px1000_cl.evt xa300041010rsl_p0px1000_uf.evt"
+    exit 1
+fi
+
+clevt=$1
+ufevt=$2
+clgti="${clevt%.evt}.gti"
+clinvgti="${clevt%.evt}_clinvgti.gti"
+ufcutevt="${ufevt%.evt}_clinvgti.evt"
+
+resolve_util_ftmgtime.sh $clevt
+gtiinvert infile=${clgti}[STDGTI] outfile=${clinvgti}
+resolve_util_ftselect.sh $ufevt "gtifilter(\"${clinvgti}\")" clinvgti
+
+resolve_util_fplot.py ${ufcutevt} TIME 1,1,1 DERIV_MAX,LO_RES_PH,RISE_TIME 1,1,1 --filters "ITYPE==4,PIXEL==0"
+resolve_util_fplot.py ${ufcutevt} DERIV_MAX 1,1 LO_RES_PH,RISE_TIME 1,1 --filters "ITYPE==4,PIXEL==0"
+
+resolve_util_fplot.py ${ufcutevt} TIME 1,1,1 DERIV_MAX,LO_RES_PH,RISE_TIME 1,1,1 --filters "ITYPE==4,PIXEL==1"
+resolve_util_fplot.py ${ufcutevt} DERIV_MAX 1,1 LO_RES_PH,RISE_TIME 1,1 --filters "ITYPE==4,PIXEL==1"
